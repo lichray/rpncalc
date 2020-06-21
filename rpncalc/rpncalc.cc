4147dbaee10e7ad27121373ad4994508cc329fe8
Zhihao Yuan <lichray@gmail.com>
2020-06-21 02:37:46 -0500

    Allow printing errors and answers.

--- rpncalc/rpncalc.cc
+++ rpncalc/rpncalc.cc
@@ -1,5 +1,23 @@
 ﻿#include "rpncalc.h"
 
+#include <charconv>
+
+void rpn::calculator::error(std::string_view msg) const
+{
+    std::string s = "error: ";
+    s.append(msg);
+    s.push_back('\n');
+    write_(s.data(), s.size());
+}
+
+void rpn::calculator::print(double val) const
+{
+    char buf[20];
+    auto [ptr, ec] = std::to_chars(std::begin(buf), std::end(buf), val);
+    *ptr = '\n';
+    write_(buf, ptr - buf);
+}
+
 void rpn::calculator::enter(std::string_view line)
 {
 }
--- rpncalc/rpncalc.h
+++ rpncalc/rpncalc.h
@@ -11,6 +11,9 @@ class calculator
     typedef void write_sig(char const*, size_t);
     std::function<write_sig> write_;
 
+    void error(std::string_view msg) const;
+    void print(double) const;
+
   public:
     explicit calculator(std::function<write_sig> fn) : write_(std::move(fn)) {}
     void enter(std::string_view line);
